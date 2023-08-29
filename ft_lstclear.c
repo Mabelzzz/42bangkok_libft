018d5893bff18355cbda51498608eff131125afa
Mabel <mobile44.mm@gmail.com>
2023-08-30 03:19:27 +0700

    fix and then So What?

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstclear.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pnamwayk <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: pnamwayk <pnamwayk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/15 23:11:28 by pnamwayk          #+#    #+#             */
-/*   Updated: 2022/08/15 23:13:08 by pnamwayk         ###   ########.fr       */
+/*   Updated: 2023/08/30 03:17:19 by pnamwayk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,12 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	t_list	*tmp;
 
-	tmp = *lst;
-	if (!tmp)
+	if (!lst || !del)
 		return ;
-	while (tmp != NULL)
+	while (*lst)
 	{
-		del(tmp->content);
-		free(tmp);
-		tmp = tmp -> next;
+		tmp = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		*lst = tmp;
 	}
-	*lst = tmp;
 }
